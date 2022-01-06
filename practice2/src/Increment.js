77243f6b471b7470b9595034ee172cad2219ead0
Muhammad-Hamza-Tahir <hamzatahir8935@gmail.com>
2022-01-06 12:04:48 +0500

    Create Increment.js

---
+++ practice2/src/Increment.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+
+export default function Increment() {
+  return (
+      <>
+    <div className='main'>
+      <h1 className='result'>0</h1>
+      <button>Click me</button>
+    </div>
+   
+        
+    
+    </>
+  )
+}
