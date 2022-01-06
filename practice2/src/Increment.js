2c9c9043bc713e6fcd443a35eb92be230ebaee79
Muhammad-Hamza-Tahir <hamzatahir8935@gmail.com>
2022-01-06 13:12:44 +0500

    Update Increment.js

--- practice2/src/Increment.js
+++ practice2/src/Increment.js
@@ -1,30 +1,30 @@
-import React, { useState } from 'react'
-import './App.css'
+// import React, { useState } from 'react'
+// import './App.css'
 
 
-export default function Increment() {
+// export default function Increment() {
  
-    let [count , setCount] = useState(0);
+//     let [count , setCount] = useState(0);
 
-let IncreFun = () =>{
-   setCount(count+ 1)
-};
-let DecFun = () =>{
-    setCount(count - 1)
-}
+// let IncreFun = () =>{
+//    setCount(count+ 1)
+// };
+// let DecFun = () =>{
+//     setCount(count - 1)
+// }
 
-  return (
-      <>
-    <div className='main'>
-      <h1 className='result'>{count}</h1>
-      <div>
-      <button onClick={IncreFun}>Click me</button>
-      <button onClick={DecFun}>Click me</button>
-      </div>
-    </div>
+//   return (
+//       <>
+//     <div className='main'>
+//       <h1 className='result'>{count}</h1>
+//       <div>
+//       <button onClick={IncreFun}>Click me</button>
+//       <button onClick={DecFun}>Click me</button>
+//       </div>
+//     </div>
    
         
     
-    </>
-  )
-}
+//     </>
+//   )
+// }
